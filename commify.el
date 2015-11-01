6ced190668e42f20ddd4799f7de9704024b29734
Daniel E. Doherty <ded-law@ddoherty.net>
2015-11-01 16:11:35 -0600

    Add license and github-recommended files.

--- commify.el
+++ commify.el
@@ -2,10 +2,23 @@
 
 ;; Copyright (C) 2015 Daniel E. Doherty
 
+;; This program is free software: you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by the Free
+;; Software Foundation, either version 3 of the License, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful, but WITHOUT
+;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+;; FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+;; more details.
+
+;; You should have received a copy of the GNU General Public License along
+;; with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 ;; Author: Daniel E. Doherty <ded-commify@ddoherty.net>
-;; Version: 1.0
-;; Package-Requires: ((s "1.0"))
-;; Keywords: numbers, grouping, commas
+;; Version: 20151101
+;; Package-Requires: ((s "20150825.50"))
+;; Keywords: convenience, editing, numbers, grouping, commas
 ;; URL: http://github.com/ddoherty03/commify
 
 ;;; Commentary:
