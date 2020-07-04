0511bd467620608fe268700fc96a3719dbf7c4de
Daniel E. Doherty <ded-law@ddoherty.net>
2020-07-04 10:09:48 -0500

    Add tests for zero and negative group size and group-char param.

--- test/commify-test.el
+++ test/commify-test.el
@@ -35,11 +35,30 @@
   (should (equal "42_34234_23483_14159" (commify--commas "42342342348314159" "_" 5)))
   (should (equal "987" (commify--commas "987" "_" 4)))
   (let ((commify-group-size 5))
-    (should (equal "42_34234_23483_14159" (commify--commas "42342342348314159" "_")))
-  ))
+    (should (equal "42_34234_23483_14159" (commify--commas "42342342348314159" "_")))))
+
+(ert-deftest commify-test-decimal-group-size-one ()
+  "Test rendering of decimal number with group of 1"
+  (should (equal "4_2_3_4_2_3_4_2_3_4_8_3_1_4_1_5_9"
+                 (commify--commas "42342342348314159" "_" 1)))
+  (should (equal "9_8_7" (commify--commas "987" "_" 1)))
+  (should (equal "9" (commify--commas "9" "_" 1))))
 
 (ert-deftest commify-test-decimal-group-size-small ()
   "Test rendering of decimal number with group size less than 1"
   (should (equal "42342342348314159" (commify--commas "42342342348314159" "_" 0)))
   (should (equal "42342342348314159" (commify--commas "42342342348314159" "_" -1)))
   (should (equal "987" (commify--commas "987" "_" 0))))
+
+;;; Stripping away the group char
+
+(ert-deftest commify-uncommify-test-decimal ()
+  "Test removal of default group-char"
+  (should (equal "8314159" (commify--uncommas "8,314,159")))
+  (should (equal "987" (commify--uncommas "987"))))
+
+(ert-deftest commify-uncommify-test-decimal-group-char ()
+  "Test removal of non-default group char"
+  (let ((commify-group-char "_"))
+    (should (equal "8314159" (commify--uncommas "8_314_159")))
+    (should (equal "987" (commify--uncommas "987" "_")))))
