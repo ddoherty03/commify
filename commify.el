d14cde94a8b542bd355a9f63c3f57f2322ae9756
Daniel E. Doherty <ded@ddoherty.net>
2026-09-01 15:35:33 -0500

    Bump patch level.

--- commify.el
+++ commify.el
@@ -18,7 +18,7 @@
 ;; with this program.  If not, see <http://www.gnu.org/licenses/>.
 ;;
 ;; Author: Daniel E. Doherty <ded-commify@ddoherty.net>
-;; Version: 1.3.6
+;; Version: 1.3.7
 ;; Package-Requires: ((s "1.9.0"))
 ;; Keywords: convenience, editing, numbers, grouping, commas
 ;; URL: https://github.com/ddoherty03/commify
@@ -62,7 +62,7 @@
 
 ;;;; Customize options.
 
-(defvar commify-version "1.3.6")
+(defvar commify-version "1.3.7")
 
 (defgroup commify nil
   "Toggle insertion of commas in numbers in buffer."
